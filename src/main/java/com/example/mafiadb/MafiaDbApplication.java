de0296dd30e713603f3c4c4744529a20ef797630
Farukh <excommunicad00@yandex.ru>
2024-03-13 18:02:12 +0600

    first commit

---
+++ src/main/java/com/example/mafiadb/MafiaDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.mafiadb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MafiaDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MafiaDbApplication.class, args);
+    }
+
+}
